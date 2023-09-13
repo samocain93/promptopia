7ee111b7fe147d1108c1219c216bd3ddb1940504
Sam O'Cain <samocain93@gmail.com>
2023-09-13 17:48:42 -0500

    added header and logos, more styling

--- app/page.jsx
+++ app/page.jsx
@@ -13,7 +13,7 @@ const Home = () => {
         discover, create, and share creative prompts{' '}
       </p>
 
-      {/* FEED */}
+      <Feed />
     </section>
   );
 };
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const Feed = () => {
   return (
     <div>
-      
+      Feed
     </div>
   )
 }
 
 export default Feed
+ 
