c173a482f3eae94d9c8f1911e5d39d4d3c0aaf50
Sam O'Cain <samocain93@gmail.com>
2023-09-17 19:13:27 -0500

    User model set up and linked with mongodb database

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,19 +2,36 @@ import NextAuth from 'next-auth/next';
 import GoogleProvider from 'next-auth/providers/google';
 import { signIn } from 'next-auth/react';
 
+import { connectToDB } from '@utils/database';
+
+// console.log({
+//   clientId: process.env.GOOGLE_ID,
+//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+// });
+
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: '',
-            clientSecret: '',
-        })
-    ],
-    async session({ session }) {
-        
-    },
-    async signIn({ profile}) {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  async session({ session }) {},
+  async signIn({ profile }) {
+    try {
+        await connectToDB()
 
+        // check if user exists
+
+
+        // if not, create a new user
+
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
     }
-})
+  },
+});
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
