d2ae992d76108e2c7c557ae44e4ebc53e0d13f70
Sam O'Cain <samocain93@gmail.com>
2023-09-19 17:14:47 -0500

    create post form added

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import Link from 'next/link';
 
-const Form = () => {
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className='w-full max-w-full flex-start flex-col'>
+      <h1 className='head_text text-left'>
+        {type} <span className='blue_gradient'>Post</span>
+      </h1>
+      <p className='desc text-left max-w-md'>
+        {type} and share amazing prompts with the world, and let your
+        imagination run wild with any AI-powered platform.
+      </p>
 
-export default Form
+      <form
+        onSubmit={handleSubmit}
+        className='mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism'
+      >
+        <label>
+          <span className='font-satoshi font-semibold text-base text-gray-700'>
+            Your AI prompt
+          </span>
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            placeholder='Write your prompt here...'
+            required
+            className='form_textarea'
+          />
+        </label>
+        <label>
+          <span className='font-satoshi font-semibold text-base text-gray-700'>
+            Tag { ` `}
+            <span className='font-normal'>(#product, #webdev, #python)</span>
+          </span>
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            placeholder='Write your prompt here...'
+            required
+            className='form_textarea'
+          />
+        </label>
+      </form>
+    </section>
+  );
+};
+
+export default Form;
